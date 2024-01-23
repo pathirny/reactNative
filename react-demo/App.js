c630b0666f677690545913152ab3fa7dbb0a2ebe
Patryk Hirny <pathirny@googlemail.com>
2024-01-23 12:25:21 +0000

    learning

--- react-demo/App.js
+++ react-demo/App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import { StyleSheet, Text, View, Button, Switch } from "react-native";
 import React, { useState } from "react";
 import Header from "./components/Header.js";
 
 export default function App() {
   const [bgColor, setBgColor] = useState("red");
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const toggleColor = () => setBgColor((previousState) => !previousState);
   return (
     <View style={[styles.container, { backgroundColor: bgColor }]}>
       <Header />
       <Text>HELLOOOOO</Text>
       <Button
-        title="Click me"
+        title="click"
         onPress={() => {
-          setBgColor("blue");
+          toggleColor("red");
         }}
       ></Button>
+
+      <Switch
+        trackColor={{ false: "black", true: "white" }}
+        thumbColor={isEnabled ? "white" : "red"}
+        onValueChange={() => {
+          toggleColor();
+        }}
+        value={isEnabled}
+      ></Switch>
       <StatusBar style="auto" />
     </View>
   );
