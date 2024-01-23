d99f7c2e49ce6db67ffa2cfc8a12a4ab1510594b
Patryk Hirny <pathirny@googlemail.com>
2024-01-23 11:58:27 +0000

    playing wiht react native dom

--- react-demo/App.js
+++ react-demo/App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Header from "./components/Header.js"
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import React, { useState } from "react";
+import Header from "./components/Header.js";
+
 export default function App() {
+  const [bgColor, setBgColor] = useState("red");
   return (
-    <View style={styles.container}>
-      <Header/>
+    <View style={[styles.container, { backgroundColor: bgColor }]}>
+      <Header />
+      <Text>HELLOOOOO</Text>
+      <Button
+        title="Click me"
+        onPress={() => {
+          setBgColor("blue");
+        }}
+      ></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +23,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
--- react-demo/components/Header.js
+++ react-demo/components/Header.js
@@ -1,6 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Header(){
-return(
-<Text>THis is a header</Text>
-)}
+export default function Header() {
+  return <Text style={styles.header}>THis is a header</Text>;
+}
+
+const styles = StyleSheet.create({
+  header: {
+    color: "pink",
+    backgroundColor: "orange",
+    fontSize: 40,
+  },
+});
